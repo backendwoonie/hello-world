1bed99bdc6848bd3a81b7b3e6a4dfabbb99f75ae
^_^ <^_^@w>
2021-12-04 02:25:39 +0900

    hi world 메시지 추가

--- src/helloworld.java
+++ src/helloworld.java
@@ -3,6 +3,8 @@ public class helloworld {
  public static void main(String[] args) {
 	 System.out.println("Hello World");
 	 System.out.println("Kumusta ka");
+	 System.out.println("Hi World");
+	 
  }
 }
  
